e364771948503700919c8d1d841289cfce5e6032
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-04-10 00:16:11 -0300

    feat(ComponentLogin):#LM005

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,63 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterOutlet],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  constructor(private router: Router) {}
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email')?.value;
+      const password = this.loginForm.get('password')?.value;
+
+      const userData = JSON.parse(localStorage.getItem('userData') || '{}');
+
+      if (email && userData.email === email && userData.password === password) {
+        localStorage.setItem('isLoggedIn', 'true');
+      } else {
+        alert('Usuário ou senha inválidos');
+      }
+    } else {
+      alert('Por favor, preencha todos os campos corretamente!');
+    }
+  }
+
+  onForgotPassword() {
+    const email = this.loginForm.get('email')?.value;
+
+    const userData = JSON.parse(localStorage.getItem('userData') || '{}');
+
+    if (email && userData.email === email) {
+      userData.password = '1q2w3e@!';
+      localStorage.setItem('userData', JSON.stringify(userData));
+
+      alert(
+        'Sua senha foi alterada para a senha padrão: 1q2w3e@!. Por favor, prossiga utilizando essa senha.',
+      );
+    } else {
+      alert('Email não encontrado');
+    }
+  }
+
+  onRegister() {
+    this.router.navigate(['/sign-up']);
+  }
+}
---
+++ src/app/components/login/sign-up/sign-up.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [HttpClientModule,ReactiveFormsModule,FormsModule,CommonModule],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss',
+})
+export class SignUpComponent {
+  SignUpForm: FormGroup | any;
+
+  constructor(private router: Router) {
+    this.initializeForm();
+  }
+  initializeForm() {
+    this.SignUpForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      date: new FormControl('', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
+  SignUp() {
+    if (this.SignUpForm.valid) {
+      if (this.passwordsMatch()) {
+        this.storeUserData();
+        alert('Usuário cadastrado com sucesso!');
+      } else {
+        alert('As senhas não correspondem!');
+      }
+    } else {
+      alert('Por favor, preencha todos os campos corretamente!');
+    }
+  }
+  passwordsMatch(): boolean {
+    return this.SignUpForm.value.password === this.SignUpForm.value.confirmPassword;
+  }  
+  storeUserData() {
+    let formData = { ...this.SignUpForm.value };
+    localStorage.setItem('userData', JSON.stringify(formData));
+  }
+  onLogin() {
+    this.router.navigate(['/login']);
+  }
+}
+
