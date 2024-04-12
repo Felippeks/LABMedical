9b4ad0de6a5b7278478ed8fbc0260829dbcb725d
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-04-12 11:57:22 -0300

    feat(JsonServer):ConstructorJsonServer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     importProvidersFrom([BrowserAnimationsModule]),
+    provideHttpClient()
   ],
 };
