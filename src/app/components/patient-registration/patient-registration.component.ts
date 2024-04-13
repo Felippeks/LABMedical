344af8ae80c4c1e3d870ac37b38a3f24096062cc
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-04-12 23:53:21 -0300

    feat(Registratio): #LM009.5

--- src/app/components/patient-registration/patient-registration.component.ts
+++ src/app/components/patient-registration/patient-registration.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../shareds_components/sidebar/sidebar.component';
 import { ToolbarComponent } from '../shareds_components/toolbar/toolbar.component';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CepService } from '../../services/cep.service';
-
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-patient-registration',
   standalone: true,
   templateUrl: './patient-registration.component.html',
   styleUrl: './patient-registration.component.scss',
-  imports: [SidebarComponent, ToolbarComponent],
+  imports: [SidebarComponent, ToolbarComponent, ReactiveFormsModule],
 })
 export class PatientRegistrationComponent {
   formPatient = new FormGroup({
@@ -36,31 +36,43 @@ export class PatientRegistrationComponent {
     numero: new FormControl('', Validators.required),
     complemento: new FormControl(''),
     bairro: new FormControl('', Validators.required),
-    referencia: new FormControl('')
+    referencia: new FormControl(''),
   });
 
-  onSubmit() {
-    if (this.formPatient.valid) {
-      console.log('Formulário enviado', this.formPatient.value);
-      alert('Formulário enviado com sucesso!');
-    } else {
-      alert('Por favor, preencha todos os campos obrigatórios.');
-    }
-  }
-  constructor(private cepService: CepService) { }
+  constructor(
+    private cepService: CepService,
+    private ApiService: ApiService,
+  ) {}
   onCepChange() {
     const cepControl = this.formPatient.get('cep');
     if (cepControl && cepControl.value && cepControl.value.length === 8) {
       const cep = cepControl.value;
-      this.cepService.getCepData(cep).subscribe(data => {
+      this.cepService.getCepData(cep).subscribe((data) => {
         this.formPatient.patchValue({
           logradouro: data.logradouro,
           complemento: data.complemento,
           bairro: data.bairro,
           cidade: data.localidade,
-          estado: data.uf
+          estado: data.uf,
         });
       });
+    }
+  }
+  onSubmit() {
+    console.log(this.formPatient.value)
+    if (this.formPatient.valid) {
+      console.log('Dados do paciente aqui:', this.formPatient.value);
+      this.ApiService.create('pacientes', this.formPatient.value).subscribe(
+        (data) => {
+          console.log('Paciente criado com sucesso!', data);
+          alert('Paciente criado com sucesso!');
+        },
+        (error) => {
+          console.log('Erro ao criar paciente:', error);
+        },
+      );
+    } else {
+      alert('Por favor, preencha todos os campos obrigatórios.');
     }
   }
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrls: { [key: string]: string } = {
+    pacientes: 'http://localhost:3000/pacientes',
+    consultas: 'http://localhost:3000/consultas',
+    exames: 'http://localhost:3000/exames'
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getAll(endpoint: string): Observable<any> {
+    return this.http.get(`${this.baseUrls[endpoint]}`);
+  }
+
+  get(endpoint: string, id: number): Observable<any> {
+    return this.http.get(`${this.baseUrls[endpoint]}/${id}`);
+  }
+
+  create(endpoint: string, data: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrls[endpoint]}`, data);
+  }
+
+  update(endpoint: string, id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrls[endpoint]}/${id}`, value);
+  }
+
+  delete(endpoint: string, id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrls[endpoint]}/${id}`, { responseType: 'text' });
+  }
+}
