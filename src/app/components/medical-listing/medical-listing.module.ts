cc70ad1c0364f2eb2e73cefcc029eb48b9b3ed61
Felippe Kulkamp Sant ana <123035151+Felippeks@users.noreply.github.com>
2024-04-17 21:57:32 -0300

    fix(CodeReviewFix):#LM014-RoutesChildrenFix

--- src/app/components/medical-listing/medical-listing.module.ts
+++ src/app/components/medical-listing/medical-listing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { MedicalRecordListingComponent } from './medical-listing.component';
+import { PatientMedicalListingComponent } from './patient-listing/patient-listing.component';
 
 const medicalRecordRoutes: Routes = [
   {
     path: '',
     children: [{ path: '', component: MedicalRecordListingComponent }],
   },
+  { path: 'patientListing', component: PatientMedicalListingComponent },
 ];
 
 @NgModule({
--- src/app/guardsRoutes/auth-child-guard.guard.ts
+++ src/app/guardsRoutes/auth-child-guard.guard.ts
@@ -2,14 +2,14 @@ import { CanActivateChildFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 
 export const authChildGuard: CanActivateChildFn = (childRoute, state) => {
-  {
-    const router = inject(Router);
-    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
-    const isIdNumber = !isNaN(Number(childRoute.paramMap.get('id')));
-    if (!isLoggedIn || !isIdNumber) {
+  const router = inject(Router);
+  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+  const isIdNumber = !isNaN(Number(childRoute.paramMap.get('id')));
+  if (!isLoggedIn || !isIdNumber) {
+    if (state.url !== '/login') {
       router.navigate(['/login']);
-      return false;
     }
-    return true;
+    return false;
   }
+  return true;
 };
